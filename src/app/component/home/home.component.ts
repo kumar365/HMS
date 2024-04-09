3ce6912d8051c7f9bf8f65003c5934154864e325
krvspk <krvspkumar1@gmail.com>
2024-04-09 16:07:02 +0530

    Validations changes

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/model/user';
+import { UserInfo } from 'src/app/model/user-info';
+import { StorageService } from 'src/app/service/storage.service';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  message: any;
+  statusFlag: boolean = false;
+  currentUserInfo: UserInfo = new UserInfo;
+  currentUser: User = new User;
+  constructor(private storageService: StorageService, private userService: UserService) { }
 
+  ngOnInit(): void {
+    this.currentUserInfo = this.storageService.getUser();
+    if (this.currentUserInfo != null) {
+      this.currentUserInfo.token = this.storageService.getToken();
+      this.getUserData();
+    }
+  }
+  getUserData() {
+    this.userService.getUser(this.currentUserInfo).subscribe((data: User) => {
+      this.currentUser = data;
+    });
+  }
 }
+
